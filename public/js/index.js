9bd0e62326ed48fcd787230da1b392d15c92aabd
Erik Szauer <eszauer@datasec.de>
2017-05-23 16:42:26 +0200

    Cleaned some code - Cleaned code in the public/js/index.js file

--- public/js/index.js
+++ public/js/index.js
@@ -4,40 +4,40 @@ var id;
 
 $(".delete").click(function() {
     // alert(this.id); // or alert($(this).attr('id'));
-    id = $(this).attr('id');
-    $.ajax({
-      type: 'DELETE',
-      url: '/delete-user/' + id,
-      success: function(data){
-        console.log(data);
-        location.reload();
-      }
-    });
+  id = $(this).attr('id');
+  $.ajax({
+    type: 'DELETE',
+    url: '/delete-user/' + id,
+    success: function(data){
+      console.log(data);
+      location.reload();
+    }
+  });
 });
 
 
 
 $(".edit").click(function() {
-    // alert(this.id); // or alert($(this).attr('id'));
-    id = $(this).attr('id');
-    console.log(id);
-    let username = $('div#' + id + ' h4').text();
-    let description = $('div#' + id + ' p.description').text();
-    let skill = $('div#' + id + ' p.skill').text();
+  // alert(this.id); // or alert($(this).attr('id'));
+  id = $(this).attr('id');
+  console.log(id);
+  let username = $('div#' + id + ' h4').text();
+  let description = $('div#' + id + ' p.description').text();
+  let skill = $('div#' + id + ' p.skill').text();
 
-    $('#exampleModal').on('show.bs.modal', function (event) {
-      console.log('Show modal');
-      var button = $(event.relatedTarget); // Button that triggered the modal
-      // var recipient = button.data('whatever'); // Extract info from data-* attributes
-      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-      var modal = $(this);
-      // modal.find('.modal-title').text('New message to ' + recipient);
-      // modal.find('.modal-body input').val(recipient);
-      modal.find('#user-name').val(username);
-      modal.find('#description-text').val(description);
-      modal.find('#skill-text').val(skill);
-    });
+  $('#exampleModal').on('show.bs.modal', function (event) {
+    console.log('Show modal');
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    // var recipient = button.data('whatever'); // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this);
+    // modal.find('.modal-title').text('New message to ' + recipient);
+    // modal.find('.modal-body input').val(recipient);
+    modal.find('#user-name').val(username);
+    modal.find('#description-text').val(description);
+    modal.find('#skill-text').val(skill);
+  });
 });
 
 
@@ -51,13 +51,8 @@ $(".save").click(function(){
     description: $('#description-text').val(),
     skill: $('#skill-text').val()
   };
-  let username = $('#user-name').val();
-  let description = $('#description-text').val();
-  let skill = $('#skill-text').val();
 
   // Ajax call to update user-data
-
-  console.log(id);
   $.ajax({
     type: 'PUT',
     url: '/update-user',
